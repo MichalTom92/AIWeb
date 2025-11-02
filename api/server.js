0f1969c7db96605aaf896b3c5616e0c9b995548e
MichalTom92 <mikrutcn92@gmail.com>
2025-11-02 19:07:24 +0000

    cipa

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const { HfInference } = require('@huggingface/inference');
-// Usunięto require('dotenv'), ponieważ Vercel zarządza zmiennymi środowiskowymi.
+
 
 const app = express();
 const port = 3000;
